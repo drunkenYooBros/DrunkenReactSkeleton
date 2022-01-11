6f65541f284893b3edde9acc5a59e1e21f288e13
hongsik <devhongs@gmaile.com>
2022-01-12 01:19:45 +0900

    Suspense 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import './App.css';
 import 'bulma/css/bulma.min.css';
-import Main from './layout/main/Main';
+// import Main from './layout/main/Main';
 import { useSetRecoilState } from 'recoil';
 import { MainMenuState } from 'state';
 import Rest, { RestRequestConfig } from 'service/Rest';
 
-function App() {
-  const [loading, setloadStatus] = useState(true)
-  const setMainMenuList = useSetRecoilState(MainMenuState)
-  useEffect(() => {
-    const restParams: RestRequestConfig = {
-      url: '/menu'
-    }
-    Rest.get(restParams)
-    .then((res: any) => {
-      setMainMenuList(res)
-      setloadStatus(false)
-    })
-  }, [])
-
-  if (loading) {
-    return (
-      <div className="App">
-        Loading ...
-      </div>
-    )
+async function bootstrapApp() {
+  const config = await Rest.get('/config')
+  const menus = await Rest.get('/menu')
+  // console.log(server_config)
+  return {
+    config,
+    menus
   }
+}
+
+function App() {
+
+  // const [loading, setloadStatus] = useState(true)
+  const setMainMenuList = useSetRecoilState(MainMenuState)
+  // let MainPage = (<></>)
+  const Main = React.lazy(() => {
+    return bootstrapApp().then(d => {
+      console.log(d)
+      setMainMenuList(d.menus)
+      return import('./layout/main/Main')
+    })
+  })
   
   return (
     <div className="App">
-      <Main />
+      <Suspense fallback={<>Loading ...</>}>
+        <Main />
+      </Suspense>
     </div>
   );
 }
