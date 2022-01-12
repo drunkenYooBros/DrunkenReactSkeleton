3758512bbf865ff9d4af504962ed2362dccf72f7
hongsik <devhongs@gmaile.com>
2022-01-13 00:17:04 +0900

    App.tsx <Suspense> 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import 'bulma/css/bulma.min.css';
-// import Main from './layout/main/Main';
 import { useSetRecoilState } from 'recoil';
 import { MainMenuState } from 'state';
-import Rest, { RestRequestConfig } from 'service/Rest';
-
-async function bootstrapApp() {
-  const config = await Rest.get('/config')
-  const menus = await Rest.get('/menu')
-  // console.log(server_config)
-  return {
-    config,
-    menus
-  }
-}
+import apiPortal from 'api/apiPortal';
+ 
 
 function App() {
-
-  // const [loading, setloadStatus] = useState(true)
   const setMainMenuList = useSetRecoilState(MainMenuState)
-  // let MainPage = (<></>)
-  const Main = React.lazy(() => {
-    return bootstrapApp().then(d => {
-      console.log(d)
-      setMainMenuList(d.menus)
-      return import('./layout/main/Main')
-    })
+  const Main = React.lazy(async () => {
+    const data = await apiPortal.getBootStrapData();
+    console.log(data);
+    setMainMenuList(data.menu);
+    return await import('./layout/main/Main');
   })
-  
   return (
     <div className="App">
       <Suspense fallback={<>Loading ...</>}>
