cb41c7587ade5fe8189a19f61f198d0120dd8c5d
ArnoldYoo <airnold0986@gmail.com>
2022-01-13 22:47:04 +0900

    필요없는 파일 삭제

--- src/state/InputText.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil'
-import * as States from "constant/state";
-
-export default atom<string>({
-  key: States.INPUT_TEXT,
-  default: '',
-})
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,7 +1,5 @@
 import MainMenuState from './Menu'
-import InputTextState from './InputText'
 
 export {
-    MainMenuState,
-    InputTextState
+    MainMenuState
 }
