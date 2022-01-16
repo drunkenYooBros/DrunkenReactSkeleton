48b3e8fd4a914d0a890aaed36926c82cdefd222b
ArnoldYoo <airnold0986@gmail.com>
2022-01-16 17:36:04 +0900

    app bootstarp refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import './App.css';
 import 'bulma/css/bulma.min.css';
 import { useSetRecoilState } from 'recoil';
@@ -6,24 +6,28 @@ import { MainMenuState } from 'state';
 import apiPortal from 'api/apiPortal';
 import Spinner from 'layout/spinner/Spinner';
 
-function App() {
-  let menus
+
+export const bootatrapComponent = (LazyComponent: any) => (props: any) => {
   const setMainMenuList = useSetRecoilState(MainMenuState)
-  const Main = React.lazy(async () => {
-    // menu 가져옴
-    // user 정보 필요
-    // code 정보
-    const data = await apiPortal.getBootStrapData();
-    setMainMenuList(data.menu);
-    menus = data.menu
-    return await import('./layout/main/Main');
-  })
+  useEffect(() => {
+    apiPortal.getBootStrapData().then( (data) => {
+      setMainMenuList(data.menu)
+    })
+  }, [])
   return (
-    <Suspense fallback={Spinner}>
-      <div className="App">
-        <Main/>
-      </div>
+    <Suspense fallback={<Spinner />}>
+      <LazyComponent {...props} />
     </Suspense>
+  )
+}
+
+
+function App() {
+  const Main = bootatrapComponent(React.lazy(() => import('layout/main/Main')));
+  return (
+    <>
+      <Main/>
+    </>
   );
 }
 
